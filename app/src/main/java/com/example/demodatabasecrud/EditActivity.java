faafc589ca09d5af9f96dab3a1071b80eb9be329
20025988 <20025988@myrp.edu.sg>
2021-07-15 12:28:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demodatabasecrud/EditActivity.java
@@ -0,0 +1,58 @@
+package com.example.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        TextView tvID;
+        EditText etContent;
+        Button btnUpdate, btnDelete;
+        Note data;
+
+        etContent = findViewById(R.id.etEditContent);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        tvID = findViewById(R.id.tvID);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateNote(data);
+                dbh.close();
+
+                finish();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(EditActivity.this);
+                dbh.deleteNote(data.getId());
+
+                finish();
+            }
+        });
+
+    }
+}
